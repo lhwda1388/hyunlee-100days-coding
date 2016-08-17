fd645b6104b09f9df3f96b6cd23a2da7b6ee3457
Leehyunwoo <lhwda1388@naver.com>
2016-08-17 11:24:04 +0900

    second commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var fs = require('fs');
 var path = require('path');
-
 var gulp = require('gulp');
+
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var minifycss = require('gulp-minify-css');
+var minifyhtml = require('gulp-minify-html');
+var browserSync = require('browser-sync');
 
 // Load all gulp plugins automatically
 // and attach them to the `plugins` object
@@ -162,11 +167,59 @@ gulp.task('archive', function (done) {
     done);
 });
 
+
 gulp.task('build', function (done) {
     runSequence(
-        ['clean', 'lint:js'],
+        ['clean', 'lint:js' ],
         'copy',
     done);
 });
 
-gulp.task('default', ['build']);
+gulp.task('default', ['build', 'serve', 'watch' ]);
+
+gulp.task('serve', ['build', 'watch'], function(done) {
+    browserSync({
+        open: false,
+        port: 9000,
+        server: {
+            baseDir: ['./dist'],
+            middleware: function(req, res, next) {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                next();
+            }
+        }
+    }, done);
+});
+
+//HTML 파일을 minify
+gulp.task('minifyhtml', function () {
+    return gulp.src('src/**/*.html') //src 폴더 아래의 모든 html 파일을
+        .pipe(minifyhtml()) //minify 해서
+        .pipe(gulp.dest('dist')) //dist 폴더에 저장
+        .pipe(browserSync.reload({stream:true})); //browserSync 로 브라우저에 반영
+});
+
+//자바스크립트 파일을 minify
+gulp.task('uglify', function () {
+    return gulp.src('src/**/*.js') //src 폴더 아래의 모든 js 파일을
+        .pipe(concat('main.js')) //병합하고
+        .pipe(uglify()) //minify 해서
+        .pipe(gulp.dest('dist/js')) //dist 폴더에 저장
+        .pipe(browserSync.reload({stream:true})); //browserSync 로 브라우저에 반영
+});
+
+//CSS 파일을 minify
+gulp.task('minifycss', function () {
+    return gulp.src('src/**/*.css') //src 폴더 아래의 모든 css 파일을
+        .pipe(concat('main.css')) //병합하고
+        .pipe(minifycss()) //minify 해서
+        .pipe(gulp.dest('dist/css')) //dist 폴더에 저장
+        .pipe(browserSync.reload({stream:true})); //browserSync 로 브라우저에 반영
+});
+
+//파일 변경 감지
+gulp.task('watch', function () {
+    gulp.watch('src/**/*.js', ['uglify']);
+    gulp.watch('src/**/*.css', ['minifycss']);
+    gulp.watch('src/**/*.html', ['minifyhtml']);
+});
